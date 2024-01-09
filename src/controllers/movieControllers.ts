2a993ad0d7fb57f1dd75d13dc4545a8c6f3de3df
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-09 17:08:18 -0300

    work in progress: adicionando códigos.

--- src/controllers/movieControllers.ts
+++ src/controllers/movieControllers.ts
@@ -8,5 +8,11 @@ import { movieModel } from "../models/Movie";
 import Logger from "../../config/logger";
 
 export async function createMovie(req: Request, res: Response) {
-    return res.status(200).send("Deu certo o controller")
+    try {
+        const data = req.body;
+        const movie = await movieModel.create(data);
+        return res.status(201).json(movie);
+    } catch (e: any) {
+        Logger.error(`Erro no sistema: ${e.message}`)
+    }
 }
